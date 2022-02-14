1f22926b1d745a8d7893757e982db326f0d5c13d
Chris Bricker <csbricker8@gmail.com>
2022-02-13 22:24:47 -0700

    Add authentication configuration

--- apps/firebase-sandbox/src/app/app.module.ts
+++ apps/firebase-sandbox/src/app/app.module.ts
@@ -1,13 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import {
+    connectFirestoreEmulator,
+    enableIndexedDbPersistence,
+    getFirestore,
+    provideFirestore
+} from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { FirebaseUIModule, firebase } from 'firebaseui-angular';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([]),
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideFirestore(() => {
+            const firestore = getFirestore();
+            connectFirestoreEmulator(firestore, 'localhost', 8080);
+            enableIndexedDbPersistence(firestore);
+            return firestore;
+        }),
+        provideAuth(() => getAuth()),
+        FirebaseUIModule.forRoot({
+            signInFlow: 'popup',
+            signInOptions: [
+                firebase.auth.EmailAuthProvider.PROVIDER_ID
+            ]
+        })
+
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- apps/firebase-sandbox/src/environments/environment.prod.ts
+++ apps/firebase-sandbox/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true,
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyC0NH6en2QZk_mR9BZSdwgE5sRFUb0vSFg",
+        authDomain: "sandbox-4df2d.firebaseapp.com",
+        projectId: "sandbox-4df2d",
+        storageBucket: "sandbox-4df2d.appspot.com",
+        messagingSenderId: "834712794018",
+        appId: "1:834712794018:web:bed48ca5832fb2c24c38fb",
+        measurementId: "G-MLEBVCYDH7",
+    },
 };
