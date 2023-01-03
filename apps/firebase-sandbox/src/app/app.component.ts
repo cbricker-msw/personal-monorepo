d9c6530454900f7dbae6cae2132564ebe245e9a6
Chris Bricker <csbricker8@gmail.com>
2023-01-03 14:41:06 -0700

    Updates for formatting and packages

--- apps/firebase-sandbox/src/app/app.component.ts
+++ apps/firebase-sandbox/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'personal-monorepo-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'personal-monorepo-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebase-sandbox';
+    title = 'firebase-sandbox';
 }
